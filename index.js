141a94900f93cbedc20b6659a8db6276f125f4e5
Heatzeker <hueblerezekiel@gmail.com>
2022-05-18 16:23:48 -0500

    fixes to index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { Pool } = require('pg/lib');
-const { connectionString } = require('pg/lib/defaults');
-const Port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 
 const pool = new Pool({
