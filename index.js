47159d0e7a63181f31776b1b253f8fc6b8e26d38
Heatzeker <hueblerezekiel@gmail.com>
2022-05-18 22:22:40 -0500

    Add /db-info route with table display

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.Databse_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
@@ -26,6 +26,34 @@ express()
         catch (err) {
             console.error(err);
             res.send("Error" + err);
+        }
+    })
+    .get('/db-info', async(req, res) => {
+        try {
+            const client = await pool.connect();
+            const tables = await client.query(
+                `SELECT c.relname AS table, a.attname as column, 
+                t.typname As type
+                FROM pg_catalog.pg_class AS c
+                LEFT JOIN pg_catalog.pg_attribute AS a 
+                ON c.oid = a.attrelid AND a.attnum > 0
+                LEFT JOIN pg_catalog.pg_type AS t
+                ON a.atttypid = t.oid
+                WHERE c.relname IN ('users', 'observations', 'students',
+                'schools', 'tasks')
+                ORDER BY c.relname, a.attnum;
+                `);
+
+                const locals = {
+                    'tables': (tables) ? tables.rows : null
+                };
+
+                res.render('pages/db-info', locals);
+                client.release();
+        }
+        catch (err) {
+            console.error(err);
+            res.send("Error: " + err);
         }
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
